8286662eeb15fed7a86adf5ddea50d2df3bd82ab
Nicoll Araujo <116590342+NicollAraujo@users.noreply.github.com>
2024-11-17 14:56:52 -0500

    Delete src directory

--- src/app/model/mascota.ts
+++
@@ -1,9 +0,0 @@
-export interface Mascota {
-    idMascota: number;
-    nombreMascota: string;
-    especie: string;
-    edad: number;
-    usuario_id: number;  // Foreign key
-    historialMedico: string;  // Example field for medical history
-  }
-  
